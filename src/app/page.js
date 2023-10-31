109dcdb495c3f4e07adda31f9b4515d7e3cada3b
Mani Kant Sharma <dadzheromani@gmail.com>
2023-11-01 01:54:21 +0530

    ff

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Chat from "./chat/page";
 
 export default function Home() {
   return (
-    <main className="flex !bg-white min-h-screen flex-col items-center justify-between p-24">
+   
       <Chat />
-    </main>
+   
   );
 }
