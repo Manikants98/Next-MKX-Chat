171b2bd694f77ab39c5e5d9dc3554dcdfc9ddba5
Mani Kant Sharma <dadzheromani@gmail.com>
2023-11-03 02:28:43 +0530

    mkx

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -10,13 +10,85 @@ import {
 } from "@mui/material";
 import Image from "next/image";
 import Options from "../pages/Options/page";
-import { whatsappChatList } from "../mock/page";
 import { useState } from "react";
 
 const Chat = () => {
   const [isUnseenMessage, setIsUnseenMessage] = useState(false);
   const [search, setSearch] = useState("");
   const [selectedChat, setSelectedChat] = useState(null);
+  const whatsappChatList = [
+    {
+      contactName: "John Doe",
+      lastMessage: "Hi there!",
+      timestamp: "10:30 AM",
+      unreadCount: 2,
+      avatar: "john-avatar.jpg",
+    },
+    {
+      contactName: "Alice Smith",
+      lastMessage: "Hello!",
+      timestamp: "Yesterday",
+      unreadCount: 0,
+      avatar: "alice-avatar.jpg",
+    },
+    {
+      contactName: "Bob Johnson",
+      lastMessage: "What are you up to?",
+      timestamp: "Monday",
+      unreadCount: 1,
+      avatar: "bob-avatar.jpg",
+    },
+    {
+      contactName: "Emily Brown",
+      lastMessage: "See you later!",
+      timestamp: "Sunday",
+      unreadCount: 0,
+      avatar: "emily-avatar.jpg",
+    },
+    {
+      contactName: "Michael Wilson",
+      lastMessage: "Good morning!",
+      timestamp: "Sunday",
+      unreadCount: 3,
+      avatar: "michael-avatar.jpg",
+    },
+    {
+      contactName: "Sophia Davis",
+      lastMessage: "Sure thing!",
+      timestamp: "Saturday",
+      unreadCount: 0,
+      avatar: "sophia-avatar.jpg",
+    },
+    {
+      contactName: "Daniel Lee",
+      lastMessage: "I'm on my way.",
+      timestamp: "Saturday",
+      unreadCount: 2,
+      avatar: "daniel-avatar.jpg",
+    },
+    {
+      contactName: "Olivia Martinez",
+      lastMessage: "Thanks a lot!",
+      timestamp: "Friday",
+      unreadCount: 0,
+      avatar: "olivia-avatar.jpg",
+    },
+    {
+      contactName: "James Garcia",
+      lastMessage: "Happy birthday!",
+      timestamp: "Friday",
+      unreadCount: 1,
+      avatar: "james-avatar.jpg",
+    },
+    {
+      contactName: "Ava Hernandez",
+      lastMessage: "Let's meet up!",
+      timestamp: "Thursday",
+      unreadCount: 0,
+      avatar: "ava-avatar.jpg",
+    },
+  ];
+
   return (
     <div className="min-h-screen bg-white dark:bg-[#0c1317] flex lg:p-5">
       <div className="flex lg:flex-row flex-col shadow w-full bg-gray-100 dark:bg-[#222e35]">
