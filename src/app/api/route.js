f44cfdbbc9270eec49d3febe4e19f66673e9b654
Mani Kant Sharma <dadzheromani@gmail.com>
2023-11-04 20:42:02 +0530

    mkx

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 import { whatsappChatList } from "./chat";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+
 export async function GET(req, res) {
-  const allUsers = await prisma.users.findMany();
   return NextResponse.json(
-    { message: "Hello World", data: whatsappChatList, user: allUsers },
+    {
+      message: "Chat list get successfully",
+      data: whatsappChatList,
+      user: allUsers,
+    },
     { status: 200 }
   );
 }
